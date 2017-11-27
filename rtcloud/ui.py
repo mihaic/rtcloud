375744b6a14220ee8b51a9ab4d556520f94da534
Mihai Capotă <mihai@mihaic.ro>
2017-11-26 21:10:56 -0800

    Clear and display

--- rtcloud/ui.py
+++ rtcloud/ui.py
@@ -5,6 +5,7 @@ from IPython import display
 def display_input(nifti, i, fig, ax, cut_coords=None):
     if cut_coords is None:
         cut_coords = [-9]
+    ax.clear()
     plotting.plot_img(nifti, title="In {}".format(i), axes=ax,
                       display_mode="z", cut_coords=cut_coords)
     display.clear_output(wait=True)
@@ -17,3 +18,5 @@ def display_output(nifti, i, fig, ax, cut_coords=None):
     ax.clear()
     plotting.plot_img(nifti, title="Out {}".format(i), axes=ax,
                       display_mode="z", cut_coords=cut_coords)
+    display.clear_output(wait=True)
+    display.display(fig)
